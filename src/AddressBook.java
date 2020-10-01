8d4cd30420fcd6053ae4b5466809e09174fb64f5
wyosief19 <wyosief19@gmail.com>
2020-10-01 19:01:05 -0400

    Updated the main to include a second buddy.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -26,9 +26,11 @@ public class AddressBook {
     public static void main(String[] args){
         System.out.println("Address book");
         BuddyInfo buddy1 = new BuddyInfo("Lola", "123 Happy Crescent", "613 256 4321");
+        BuddyInfo buddy2 = new BuddyInfo("Shawn", "456 California Avenue", "613 279 8963");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy1);
         addressBook.removeBuddy(0);
+
     }
 
 
