6a6ad44ff15d4d2b39a56d1302eb3c59ae9ecdb1
wyosief19 <wyosief19@gmail.com>
2020-10-01 18:52:03 -0400

    Updated addBuddy and removeBuddy.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -8,20 +8,27 @@ public class AddressBook {
         buddies = new ArrayList<>();
     }
 
-    public void addBuddy(BuddyInfo buddy){
-        buddies.add(buddy);
+    public void addBuddy(BuddyInfo buddy) {
+        if (buddy != null) {
+            buddies.add(buddy);
+        }
     }
 
-    public void removeBuddy(BuddyInfo buddy){
-        buddies.remove(buddy);
+    public BuddyInfo removeBuddy(int index){
+
+        if(index >= 0 && index < buddies.size()) {
+            return buddies.remove(index);
+        }
+        return null;
+
     }
 
     public static void main(String[] args){
         System.out.println("Address book");
-        BuddyInfo buddy = new BuddyInfo("Lola", "123 Happy Crescent", "613 256 4321");
+        BuddyInfo buddy1 = new BuddyInfo("Lola", "123 Happy Crescent", "613 256 4321");
         AddressBook addressBook = new AddressBook();
-        addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
+        addressBook.addBuddy(buddy1);
+        addressBook.removeBuddy(0);
     }
 
 
